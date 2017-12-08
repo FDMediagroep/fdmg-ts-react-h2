a15adcaea6e4e0c6ab87bc2b870660159e91c216
Willem Liu <willem_liu@hotmail.com>
2017-12-08 05:57:25 +0100

    Test suite

--- test/H2.test.tsx
+++ test/H2.test.tsx
@@ -4,30 +4,32 @@ import H2 from '../src/H2';
 
 console.info = () => {};
 
-test('H2 renders correctly', () => {
-    let component = renderer.create(
-        <H2/>
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+describe('H2', () => {
+    test('renders correctly', () => {
+        let component = renderer.create(
+            <H2/>
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
 
-    component = renderer.create(
-        <H2>test h2</H2>
-    );
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+        component = renderer.create(
+            <H2>test h2</H2>
+        );
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
 
-    component = renderer.create(
-        <H2><span>testing 123</span><div>testing 456</div></H2>
-    );
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
+        component = renderer.create(
+            <H2><span>testing 123</span><div>testing 456</div></H2>
+        );
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 
-test('H2 renders correctly with CSS class', () => {
-    const component = renderer.create(
-        <H2 className={'title'}>test h2</H2>
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    test('renders correctly with CSS class', () => {
+        const component = renderer.create(
+            <H2 className={'title'}>test h2</H2>
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
